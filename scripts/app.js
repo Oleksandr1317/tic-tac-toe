1e812851a3003d32cd3e1369f58250cd0acac306
general <sgladkoskoka@gmail.com>
2024-05-15 20:59:20 +0300

    Finished login edit player name and store this value

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
+let editedPlayer = 0;
+
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    }
+];
+
 const playerConfigOverlayElement = document.getElementById(`config-overlay`)
 const backdropElement = document.getElementById(`backdrop`);
+const formElement = document.querySelector(`form`)
+const errorsElement = document.getElementById(`config-errors`)
 
 const cancelConfigButtonElement = document.getElementById(`cancel-config-button`)
 
@@ -11,3 +26,5 @@ editPlayer2BtnElement.addEventListener(`click`,openPlayerConfig);
 
 cancelConfigButtonElement.addEventListener(`click`,closePlayerConfig);
 backdropElement.addEventListener(`click`,closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig)
